28f00858ea04a1e3cf9ee3eedeeddb6724a62cd2
Jean-Baptiste Sauvan <jean-baptiste.sauvan@cern.ch>
2016-03-01 08:38:03 +0100

    Add standard background estimation in the high mT region for W+jet fit

--- CMGTools/H2TauTau/plotting/mt/plot_cfg_mutau_standardBackgroundEstimation.py
+++ CMGTools/H2TauTau/plotting/mt/plot_cfg_mutau_standardBackgroundEstimation.py
@@ -13,10 +13,12 @@ from CMGTools.H2TauTau.proto.plotter.binning import binning_svfitMass_finer
 
 int_lumi = 2094.2 # from Alexei's email
 
-#total_weight = 'weight * ' + getPUWeight()
+## Output
+version  = 'v160229'
+plot_dir = "signalRegion/StandardBackground_FromTree/{VERSION}/"
+
 total_weight = 'weight'
 
-print total_weight
 
 cuts = {}
 
@@ -25,66 +27,20 @@ inc_cut += '&& l2_decayModeFinding'
 
 cuts['inclusive'] = inc_cut + '&& l1_charge != l2_charge && !(met_pt < 0.15 && met_phi > 0. && met_phi < 1.8)'
 
-cuts['lowMT'] = cuts['inclusive'] + '&& mt < 40'
-# cuts['verylowMT'] = cuts['inclusive'] + '&& mt < 5'
+#cuts['lowMT'] = cuts['inclusive'] + '&& mt < 40'
+cuts['highMT'] = cuts['inclusive'] + '&& mt > 70'
 
-# cuts['inclusive_taumva_mupuppi'] = cuts['inclusive'].replace('l2_byCombinedIsolationDeltaBetaCorr3Hits>1.5', 'l2_byIsolationMVA3oldDMwLT>4.5').replace('l1_reliso05<0.1', '(l1_puppi_iso04_pt + l1_puppi_no_muon_iso04_pt)/l1_pt<0.284')
-
-# cuts['lowMT'] = cuts['inclusive'] + '&& mt < 40'
-# cuts['highMT'] = cuts['inclusive'] + '&& mt > 40'
-
-# cuts['inclusive_mupuppi'] = cuts['inclusive'].replace('l1_reliso05<0.1', '(l1_puppi_iso04_pt + l1_puppi_no_muon_iso04_pt)/l1_pt<0.284')
-# cuts['inclusive_taupuppi'] = cuts['inclusive'].replace('l2_byCombinedIsolationDeltaBetaCorr3Hits>1.5', 'l2_puppi_iso_pt<1.8')
-
-# cuts['inclusive_taumva'] = cuts['inclusive'].replace('l2_byCombinedIsolationDeltaBetaCorr3Hits>1.5', 'l2_byIsolationMVA3oldDMwLT>4.5')
-
-# cuts['inclusive_taumva_mupuppi'] = cuts['inclusive'].replace('l2_byCombinedIsolationDeltaBetaCorrRaw3Hits<1.5', 'l2_byIsolationMVA3oldDMwLTraw>0.848').replace('l1_reliso05<0.1', '(l1_puppi_iso04_pt + l1_puppi_no_muon_iso04_pt)/l1_pt<0.284')
-
-# cuts['highMT_taumva_mupuppi'] = cuts['inclusive_taumva_mupuppi'] + '&& mt > 40'
-# cuts['lowMT_taumva_mupuppi'] = cuts['inclusive_taumva_mupuppi'] + '&& mt < 40'
-
-# cuts['inclusive_taupuweighted'] = cuts['inclusive'].replace('l2_byCombinedIsolationDeltaBetaCorrRaw3Hits<1.5', 'l2_byPileupWeightedIsolationRaw3Hits<1.')
-
-# del cuts['inclusive']
-# del cuts['inclusive_taumva_mupuppi']
-
-# cuts['OSlowMT'] = inc_cut + '&& l1_charge != l2_charge && mt<40'
-# cuts['SSlowMT'] = inc_cut + '&& l1_charge == l2_charge && mt<40'
-
-# cuts['OShighMT'] = inc_cut + '&& l1_charge != l2_charge && mt>40'
-# cuts['SShighMT'] = inc_cut + '&& l1_charge == l2_charge && mt>40'
-
-# new_cuts = {}
-
-# for cut in cuts:
-#     new_cuts[cut.replace('inclusive', 'SS')] = cuts[cut].replace('l1_charge != l2_charge', 'l1_charge == l2_charge')
-
-# inv_cuts = {}
-# for cut in cuts:
-#     new_cuts[cut+'invmu'] = cuts[cut].replace('l1_reliso05<0.1', 'l1_reliso05>0.1')
-#     new_cuts[cut+'invtau'] = cuts[cut].replace('l2_byCombinedIsolationDeltaBetaCorr3Hits>1.5', 'l2_byCombinedIsolationDeltaBetaCorr3Hits<1.5')
-
-
-# # cuts = inv_cuts
-
-# cuts = cuts.copy()
-# cuts.update(inv_cuts)
-# cuts.update(new_cuts)
 
 qcd_from_same_sign = True
 
-analysis_dir = '/afs/cern.ch/user/s/steggema/work/public/mt/151215/'
+analysis_dir = '/afs/cern.ch/work/j/jsauvan/public/HTauTau/Trees/mt/151215/'
 samples_mc, samples_data, samples, all_samples, sampleDict = createSampleLists(analysis_dir=analysis_dir)
 
 for sample in all_samples:
-    #sample.ana_dir = analysis_dir
-    #sample.tree_prod_name = tree_prod_name
-    ## SumWeights seems to be not correctly filled
-    ## Filled them from SkimAnalyzerCount pickle file
     setSumWeights(sample, directory='MCWeighter')
 
 if qcd_from_same_sign:
-    samples_qcdfromss = [s for s in samples if s.name != 'QCD']
+    samples_qcdfromss = [s for s in all_samples if s.name != 'QCD']
     samples_ss = copy.deepcopy(samples_qcdfromss)
 
     scale = 1.06
@@ -99,30 +55,20 @@ if qcd_from_same_sign:
 
     samples_qcdfromss.append(qcd)
 
-# Taken from Variables.py, can get subset with e.g. getVars(['mt', 'mvis'])
-variables = all_vars
-# variables = getVars(['_norm_', 'mt', 'mvis', 'l1_pt', 'l2_pt', 'l1_eta', 'l2_eta', 'n_vertices', 'n_jets', 'n_bjets'])
 
-#variables = getVars(['_norm_', 'mt', 'mvis', 'n_vertices'])
-variables = getVars(['mvis'])
-#variables = [
-    #VariableCfg(name='mvis', binning={'nbinsx':60, 'xmin':0, 'xmax':300}, unit='GeV', xtitle='m_{vis}'),
-    #VariableCfg(name='mvis', binning=binning_svfitMass_finer, unit='GeV', xtitle='m_{vis}')
-#]
+#variables = getVars(['mvis', 'mt'])
+variables = [
+    #VariableCfg(name='mvis', binning=binning_svfitMass_finer, unit='GeV', xtitle='m_{vis}'),
+    VariableCfg(name='mt', binning={'nbinsx':14, 'xmin':70, 'xmax':210}, unit='GeV', xtitle='m_{T}')
+]
 
-# variables = getVars(['_norm_', 'met_pt', 'met_phi'])
-
-# variables = getVars(['_norm_'])
-# variables = [
-#     VariableCfg(name='mvis', binning={'nbinsx':35, 'xmin':0, 'xmax':350}, unit='GeV', xtitle='m_{vis}')
-# ]
 
 outputMode = 'RECREATE'
 for cut_name in cuts:
     if qcd_from_same_sign and not 'SS' in cut_name :
         cfg_example = HistogramCfg(name='example', var=None, cfgs=samples_qcdfromss, cut=inc_cut, lumi=int_lumi, weight=total_weight)
     else:
-        cfg_example = HistogramCfg(name='example', var=None, cfgs=samples, cut=inc_cut, lumi=int_lumi, weight=total_weight)
+        cfg_example = HistogramCfg(name='example', var=None, cfgs=all_samples, cut=inc_cut, lumi=int_lumi, weight=total_weight)
         
 
     cfg_example.cut = cuts[cut_name]
@@ -132,14 +78,10 @@ for cut_name in cuts:
     for variable in variables:
         cfg_example.var = variable
         if qcd_from_same_sign:
-            qcd.var = variable # Can put into function but we will not want it by default if we take normalisations from e.g. high MT
+            qcd.var = variable
         
         plot = createHistogram(cfg_example, verbose=True)
         plot.Group('VV', ['WWTo1L1Nu2Q', 'VVTo2L2Nu', 'WZTo1L1Nu2Q', 'WZTo1L3Nu', 'WZTo2L2Q', 'WZTo3L', 'ZZTo2L2Q', 'T_tWch', 'TBar_tWch'])
-        #plot.Group('VV', ['ZZ', 'WZ', 'WW', 'T_tWch', 'TBar_tWch'])
-        # plot.Group('Single t', ['T_tWch', 'TBar_tWch', 'TToLeptons_sch', 'TToLeptons_tch'])
-        # plot.Group('ZLL', ['Ztt_ZL', 'Ztt_ZJ'], style=plot.Hist('Ztt_ZL').style)
-        HistDrawer.draw(plot, plot_dir='plots/'+cut_name)
-
-        plot.WriteDataCard(filename='datacard_mutau_qcdFromSS.root', mode=outputMode, dir='{CUT}_{VAR}'.format(CUT=cut_name, VAR=variable.name))
+        HistDrawer.draw(plot, plot_dir=plot_dir.format(VERSION=version)+'/'+cut_name, SetLogy=False)
+        plot.WriteDataCard(filename=plot_dir.format(VERSION=version)+'/datacard_mutau_standardBackground.root', mode=outputMode, dir='{SEL}_{VAR}'.format(SEL=cut_name, VAR=variable.name))
         if outputMode=='RECREATE': outputMode = 'UPDATE'
