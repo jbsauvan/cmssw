20349a2a2eea7d6165be1dcf6255a0393e9cd7ab
Samuel Webb <samuel.webb@cern.ch>
2021-02-10 16:25:18 +0100

    Add rotation function to HFNoseDetIdToModule

--- DataFormats/ForwardDetId/interface/HFNoseDetIdToModule.h
+++ DataFormats/ForwardDetId/interface/HFNoseDetIdToModule.h
@@ -1,8 +1,10 @@
 #ifndef DataFormats_ForwardDetId_HFNoseDetIdToModule_H
 #define DataFormats_ForwardDetId_HFNoseDetIdToModule_H 1
 
+#include "Geometry/HGCalCommonData/interface/HGCalGeomRotation.h"
 #include "DataFormats/ForwardDetId/interface/HFNoseDetId.h"
 #include "DataFormats/ForwardDetId/interface/HFNoseTriggerDetId.h"
+#include "L1Trigger/L1THGCal/interface/HGCalModuleDetId.h"
 #include <vector>
 
 class HFNoseDetIdToModule {
@@ -14,5 +16,9 @@ public:
   static const HFNoseDetId getModule(HFNoseTriggerDetId const& id) { return id.moduleId(); }
   std::vector<HFNoseDetId> getDetIds(HFNoseDetId const& id) const;
   std::vector<HFNoseTriggerDetId> getTriggerDetIds(HFNoseDetId const& id) const;
+  std::vector<HFNoseTriggerDetId> getTriggerDetIds(HGCalModuleDetId const& id) const;
+
+private:
+  HGCalGeomRotation geom_rotation_120_ = {HGCalGeomRotation::SectorType::Sector120Degrees};
 };
 #endif
